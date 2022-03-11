aa8d2fa479b483e66001b72beacfec81157bd0ae
Jason Howmans <jason@nosaj.io>
2022-03-11 17:39:12 +0000

    Every journey begins with a single step

---
+++ src/pages/api/apply.ts
@@ -0,0 +1,13 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<unknown>,
+) {
+  console.log('Handle save data');
+  /* [TODO] 
+   * 1. Receive form data here
+   * 3. Return the right HTTP status code to the frontend, plus any other data
+   * 3. Log the data to the console
+  */
+}
